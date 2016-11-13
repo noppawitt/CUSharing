837dcabc57a9cdbd17dd60e32d33ed57d011c029
Noppawit Thairungroj <Noppawit Thairungroj>
2016-11-14 01:15:21 +0700

    add postbar and change header(BUG)

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'cusharing';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils' , 'ngAria' ,'ngMaterial'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
--- public/modules/posts/controllers/feed.client.controller.js
+++ public/modules/posts/controllers/feed.client.controller.js
@@ -30,19 +30,36 @@ angular.module('posts').controller('FeedController',[
 			postCount : 0,
 			favourite : 0
 		};
-
-		$http.get('/statuses/is_liked_post')
-		.success(function(response){
-			$scope.isLikedPostRes = response;
-		})
-		.error(function(response){
-			$scope.error = response.message;
-		});
 		
 
 		$http.get('/statuses/feed_timeline')
 		.success(function(response){
 			$scope.timeline = response;
+
+			$http.get('/statuses/is_liked_post')
+			.success(function(response2){
+				var likedPostList = response2;
+				var postList = response;
+				//console.log(postList[0].screenName);
+				//console.log(likedPostList);
+				//$scope.isLiked = {};
+				$scope.likeTxt = [];
+				$scope.likeCount = [];
+				for(var i = 0;i<postList.length;i++){
+					$scope.likeCount[postList[i]._id] = postList[i].likeCount;
+					if(likedPostList.indexOf(postList[i]._id)>=0){
+						//isLiked[postList._id] = true;
+						$scope.likeTxt[postList[i]._id] = 'Unlike';
+					}
+					else{
+						//isLiked[postList._id]= false;
+						$scope.likeTxt[postList[i]._id] = 'Like';
+					}
+				}
+			})
+		.error(function(response){
+			$scope.error = response.message;
+		});
 		})
 		.error(function(response){
 			$scope.error = response.message;
@@ -89,6 +106,7 @@ angular.module('posts').controller('FeedController',[
 
 
 		$scope.deletePost = function(_id){
+			console.log('GG');
 
 			$http.post('/statuses/delete_post',{
 				_id:_id
@@ -121,82 +139,28 @@ angular.module('posts').controller('FeedController',[
 
 
 		$scope.likePost = function(_id){
-
-			$http.post('/statuses/like_post',{
-				_id:_id
-			})
-			.success(function(response){
-				$scope.postContent ='';
-				$scope.postSubject ='';
-				$scope.postType ='';
-				$scope.postCat ='';
-
-					$http.get('/statuses/is_liked_post')
-					.success(function(response){
-						$scope.isLikedPostRes = response;
-					})
-					.error(function(response){
-						$scope.error = response.message;
-					});
-
-					$http.get('/statuses/feed_timeline')
-					.success(function(response){
-						$scope.timeline = response;
-					})
-					.error(function(response){
-						$scope.error = response.message;
-					});
-					$scope.profile={
-						name : Authentication.user.displayName,
-						screenName: Authentication.user.username,
-						postCount : 30,
-						favourite : 20
-					};
-			})
-			.error(function(response){
-				$scope.error2= response.message;
-			});
-			
-		};
-
-
-
-		$scope.unlikePost = function(_id){
-
-			$http.post('/statuses/unlike_post',{
-				_id:_id
-			})
-			.success(function(response){
-				$scope.postContent ='';
-				$scope.postSubject ='';
-				$scope.postType ='';
-				$scope.postCat ='';
-
-				$http.get('/statuses/is_liked_post')
-				.success(function(response){
-					$scope.isLikedPostRes = response;
+			if($scope.likeTxt[_id] == 'Like'){
+				$scope.likeTxt[_id] = 'Unlike';
+				$scope.likeCount[_id]++;
+				//$scope.isLiked[_id] = true;
+				$http.post('/statuses/like_post',{
+					_id:_id
 				})
 				.error(function(response){
-					$scope.error = response.message;
+					$scope.error2= response.message;
 				});
-
-				$http.get('/statuses/feed_timeline')
-				.success(function(response){
-					$scope.timeline = response;
+			}
+			else{
+				$scope.likeTxt[_id] = 'Like';
+				$scope.likeCount[_id]--;
+				//$scope.isLiked[_id] = false;
+				$http.post('/statuses/unlike_post',{
+					_id:_id
 				})
 				.error(function(response){
-					$scope.error = response.message;
-				});	
-				$scope.profile={
-					name : Authentication.user.displayName,
-					screenName: Authentication.user.username,
-					postCount : 30,
-					favourite : 20
-				};
-			})
-			.error(function(response){
-				$scope.error2= response.message;
-			});
+					$scope.error2= response.message;
+				});
+			}
 			
 		};
 
